55e9bb9ce88e2ae769359544ac9f3529343a0494
Gaellan <doucet.mari@gmail.com>
2024-02-21 15:48:32 +0100

    basic setup

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author : Gaellan
+ * @link : https://github.com/Gaellan
+ */
+
+require "vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
